663a75cea8302cafa0bf1f03dfcf6372c8a50048
Conrado Costa <conradocostaad@yahoo.com.br>
2022-08-04 23:36:55 -0300

    Applying useState in the comments of the Post

--- 01-fundamento-react-js/src/components/Post.jsx
+++ 01-fundamento-react-js/src/components/Post.jsx
@@ -1,17 +1,18 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
+
 export function Post({ author, publishedAt, content }) {
-    // const publishedDateFormatted = new Intl.DateTimeFormat("pt-BR", {
-    //     day: "2-digit",
-    //     month: "long",
-    //     hour: "2-digit",
-    //     minute: "2-digit"
-    // }).format(publishedAt);
+    
+    const [ comments, setComments ] = useState([
+        1,
+        2
+    ])
 
     const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR
@@ -21,6 +22,11 @@ export function Post({ author, publishedAt, content }) {
         locale: ptBR,
         addSuffix: true
     })
+
+    function handleCreateNewComment() {
+        event.preventDefault();
+        setComments([...comments, comments.length+1]);
+    }
 
     return (
         <article className={styles.post}>
@@ -47,16 +53,16 @@ export function Post({ author, publishedAt, content }) {
                     })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <textarea placeholder='Deixe um comentário'></textarea>
 
                 <footer><button type='submit'>Publicar</button></footer>
             </form>
 
             <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
+                {comments.map( comment => {
+                    return <Comment/>
+                })}
             </div>
         </article>
     )
