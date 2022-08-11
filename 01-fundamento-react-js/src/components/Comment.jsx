d5fc51fd235a30ead09d7bcbcb6bfe6e2865b56d
Conrado Costa <conradocostaad@yahoo.com.br>
2022-08-11 00:34:06 -0300

    Starts implementation of deleting comments

--- 01-fundamento-react-js/src/components/Comment.jsx
+++ 01-fundamento-react-js/src/components/Comment.jsx
@@ -2,7 +2,15 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        console.log('deletar');
+
+        onDeleteComment(content);
+        
+    }
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/2254731?v=4" />
@@ -15,7 +23,7 @@ export function Comment({ content }) {
                             <time title='11 de julho às 8h00' dateTime='2022-07-11 08:00'>Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24}/>
                         </button>
                     </header>
