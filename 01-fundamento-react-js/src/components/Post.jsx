6a2c44f91e51fd465bea99da14f82afe176cfa92
Conrado Costa <conradocostaad@yahoo.com.br>
2022-08-14 00:53:41 -0300

    Validating text area for new comments

--- 01-fundamento-react-js/src/components/Post.jsx
+++ 01-fundamento-react-js/src/components/Post.jsx
@@ -31,13 +31,20 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity("");
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity("Esse campo é obrigatório");
     }
 
     function deleteComment(commentToDelete) {
         const commentsWithoutDeletedOne = comments.filter(comment => comment !== commentToDelete);
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={styles.post}>
@@ -67,12 +74,16 @@ export function Post({ author, publishedAt, content }) {
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <textarea                 
                     name="comment"
-                    placeholder='Deixe um comentário'
+                    placeHolder='Deixe um comentário'
                     value={newCommentText}
-                    onChange={handleNewCommentChange} >
-                </textarea>
+                    onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid} 
+                    required
+                />
 
-                <footer><button type='submit'>Publicar</button></footer>
+                <footer>
+                    <button type='submit' disabled={isNewCommentEmpty}>Publicar</button>
+                </footer>
             </form>
 
             <div className={styles.commentList}>
